02f03bf9a5cf8ecbf6eaec0e8f3e333dbdc1a57a
Waseef Nayeem <haxerus.corporation@gmail.com>
2025-01-01 23:38:04 -0500

    Added ndstool wrapper

--- src/main.py
+++ src/main.py
@@ -2,19 +2,62 @@ from kivy.config import Config
 Config.set("graphics", "resizable", 0)
 
 from kivy.app import App
-from kivy.app import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.popup import Popup
+
+from ndstool.ndstool_wrapper import NdsToolWrapper
 
 import os
 
-class YGORandomizer(Widget):
-    pass
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+    default_path = StringProperty(None)
 
-class RandomizerApp(App):
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+    default_path = StringProperty(None)
+
+class InfernityRandomizer(FloatLayout):
+    load_file = ObjectProperty(None)
+    save_file = ObjectProperty(None)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup, default_path=os.getcwd())
+        self._popup = Popup(title="Load ROM", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup, default_path=os.getcwd())
+        self._popup = Popup(title="Save ROM", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        rom_path = os.path.join(path, filename[0])
+        self.nds_tool = NdsToolWrapper(rom_path)
+        self.nds_tool.extract()
+        self.dismiss_popup()
+
+    def save(self, path, filename):
+        print(os.path.join(path, filename))
+        self.dismiss_popup()
+
+class RandomizerApp(App):    
     def build(self):
         # Set app properties
         self.icon = "res/icon.png"
         self.title = "Infernity Randomizer v0.1"
-        return YGORandomizer()
+        return InfernityRandomizer()
+
+Factory.register('InfernityRandomizer', cls=InfernityRandomizer)
+Factory.register('LoadDialog', cls=LoadDialog)
+Factory.register('SaveDialog', cls=SaveDialog)
 
 if __name__ == "__main__":
     RandomizerApp().run()
---
+++ src/ndstool/ndstool_wrapper.py
@@ -0,0 +1,40 @@
+import subprocess
+from pathlib import Path
+
+class NdsToolWrapper():
+    def __init__(self, rom_path):
+        self.rom_path = rom_path
+
+    def extract(self):
+        temp_dir = Path("ndstool/temp")
+        if not temp_dir.exists():
+            temp_dir.mkdir(parents=True)
+
+        args = ["ndstool\\ndstool.exe",
+                "-x", self.rom_path,
+                "-9", "ndstool\\temp\\arm9.bin",
+                "-7", "ndstool\\temp\\arm7.bin",
+                "-y9", "ndstool\\temp\\y9.bin",
+                "-y7", "ndstool\\temp\\y7.bin",
+                "-d", "ndstool\\temp\\data",
+                "-y", "ndstool\\temp\\overlay",
+                "-t", "ndstool\\temp\\banner.bin",
+                "-h", "ndstool\\temp\\header.bin"]
+        subprocess.run(args)
+    
+    def build(self, output_path):
+        temp_dir = Path("ndstool/temp")
+        if not temp_dir.exists():
+            raise RuntimeError("Extracted ROM files are missing")
+        
+        args = ["ndstool\\ndstool.exe",
+                "-c", output_path,
+                "-9", "ndstool\\temp\\arm9.bin",
+                "-7", "ndstool\\temp\\arm7.bin",
+                "-y9", "ndstool\\temp\\y9.bin",
+                "-y7", "ndstool\\temp\\y7.bin",
+                "-d", "ndstool\\temp\\data",
+                "-y", "ndstool\\temp\\overlay",
+                "-t", "ndstool\\temp\\banner.bin",
+                "-h", "ndstool\\temp\\header.bin"]
+        subprocess.run(args)
